4918003209acd00fb965f0b3e6a36277e544278e
medaminedev66 <md.amine.smahi@gmail.com>
2023-03-12 22:00:15 +0100

    Remove application form javascripts_tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'searches#search'
   get 'searches/search'
   get '/searches', to: 'searches#index'
 
-  post '/save_search', to: 'searches#create', defaults: {format: :json}
+  post '/save_search', to: 'searches#create', defaults: { format: :json }
 end
