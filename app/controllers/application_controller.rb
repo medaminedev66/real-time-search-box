14a794bfec63754dbfb157e7bbef176204c6416f
Amine Smahi <md.amine.smahi@gmail.com>
2024-01-06 14:43:41 +0100

    Handle Sign_out action. Add and style Header. Fix navigation issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
+  helper_method :current_page?
+
+  def current_page?(path)
+    request.path == path
+  end
 end
