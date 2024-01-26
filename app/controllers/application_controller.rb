0c7b266652f8c66bd2a5b56a1b6cc0ff32977928
Amine Smahi <md.amine.smahi@gmail.com>
2024-01-26 12:34:14 +0100

    Redirect users to the search page after login/signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < ActionController::Base
   def current_page?(path)
     request.path == path
   end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      searches_search_path(resource)
+    else
+      super
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    if resource.is_a?(User)
+      searches_search_path(resource)
+    else
+      super
+    end
+  end
 end
